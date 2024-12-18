d4bfb37ba65f74e369909051d4a11fa9bdd46df1
Yu-ka0912 <aoyamayuka.0912@gmail.com>
2024-12-18 23:35:35 +0900

    プログラムの記述

--- src/CheckLoadJDBC.java
+++ src/CheckLoadJDBC.java
@@ -1,9 +1,19 @@
 
 public class CheckLoadJDBC {
-
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
-	}
+	
+	 public static void main(String[] args) throws
+	 InstantiationException,IllegalAccessException{
+		 //TODO自動生成されたメソッド・スタブ
+		 String msg="";
+		 
+		 
+		 try{
+			 Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			 msg="ドライバのロードに成功しました";
+		 }catch (ClassNotFoundException e){
+			 msg="ドライバのロードに失敗しました";
+		 }
+		 System.out.println(msg);
+	 }
 
 }
