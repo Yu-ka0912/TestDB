ffa1384593e60636747379c8e1e26bcec18cb815
Yu-ka0912 <aoyamayuka.0912@gmail.com>
2025-01-22 21:33:34 +0900

    TestUserDAOにプログラミング追加①

--- src/DBConnector.java
+++ src/DBConnector.java
@@ -2,7 +2,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 public class DBConnector {
-	private static String driverName="com.mysql.jdbc.Driver";
+	private static String driverName="com.mysql.cj.jdbc.Driver";
 	
 	private static String url="jdbc:mysql://localhost/testdb?autoReconnect=true&useSSL=false";
 	
